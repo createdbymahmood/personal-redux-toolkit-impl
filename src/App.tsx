26f0121486f1ddc9d12dd8fa1cbe0bf5325f441e
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-09-03 00:46:30 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,5 @@ import { useUpdate, createBreakpoint, createMemo } from "react-use";
 console.log({ Mui, useUpdate, createBreakpoint, createMemo });
 
 export default function App() {
-    return <div>Random thing in app</div>;
+    return <div>Content updated</div>;
 }
